f1734c02d7209e7aa6ea2305a51be4d86046d998
Alan Nardo <alan.nardo.91@gmail.com>
2023-06-12 12:14:43 +0100

    moved to vue, looks like it all works. needs a quick review after i have some time away before submit

---
+++ src/components/Students.vue
@@ -0,0 +1,22 @@
+<script async setup>
+
+defineProps(['students', 'class_name'])
+
+</script>
+
+<template>
+  <h4 v-if="students.length">Students in {{ class_name }}: </h4>
+  <ul>
+    <li v-for="student in students">
+      {{ student.forename }} {{ student.surname }}
+    </li>
+    <!-- <li>Hello</li> -->
+  </ul>
+</template>
+
+<style scoped>
+
+li {
+  color: #FFD646
+}
+</style>
